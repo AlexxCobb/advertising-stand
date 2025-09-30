7c046b63be25982f2b5e3629f4cfafc883038ae4
Александр <zpeoplle@yandex.ru>
2025-09-30 20:11:46 +0300

    Initial commit

---
+++ src/main/java/github/com/AlexxCobb/advertising_stand/AdvertisingStandApplication.java
@@ -0,0 +1,13 @@
+package github.com.AlexxCobb.advertising_stand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisingStandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisingStandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/com/AlexxCobb/advertising_stand/AdvertisingStandApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.AlexxCobb.advertising_stand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisingStandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
